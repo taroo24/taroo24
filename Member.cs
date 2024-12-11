921a7c131f5d0269ab9411322af768ace5f60661
taroo24 <115445530+taroo24@users.noreply.github.com>
2024-12-11 22:39:27 +0800

    Add files via upload

---
+++ Member.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KielApp
+{
+    public class Member
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageSource { get; set; }
+        public string? Bio { get; set; }
+        public string? Hometown { get; set; }
+        public string? Birthday { get; set; }
+        public string? ZodiacSign { get; set; }
+    }
+}
