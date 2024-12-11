a151dd9c8c5f36e8f59f02f5a46815abb858ce40
taroo24 <115445530+taroo24@users.noreply.github.com>
2024-12-11 22:37:49 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace KielApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnProceedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new homePage());
+        }
+
+    }
+
+}
